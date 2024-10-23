004e4cd88cd12dfb2dcedcd544d56a6c67e6b43d
Oscar Foronda <oscarforonda01@gmail.com>
2024-10-23 16:39:12 -0500

    Se actualizó hasta el video 8 servicios de GET-PUT-POST-DELETE

--- routes/routes.php
+++ routes/routes.php
@@ -2,8 +2,8 @@
 
 $routesArray = explode("/", $_SERVER['REQUEST_URI']);
 $routesArray = array_filter($routesArray);
-//echo '<pre>'; print_r($routesArray); echo '</pre>';
 
+/* Este es el proceso cuando no se hace ninguna peticion a la API */
 if(count($routesArray) == 0){
 
     $json = array(
@@ -16,5 +16,70 @@ if(count($routesArray) == 0){
     return;
 
 }
+
+/* Este es el proceso cuando si se hace una peticion a la API */
+if(count($routesArray) == 1 && isset($_SERVER['REQUEST_METHOD'])){
+
+    /* Petición GET */
+
+    if($_SERVER['REQUEST_METHOD'] == "GET"){
+
+        $json = array(
+
+            'status' => 202,
+            'result' => 'Solicitud GET'
+        
+        );
+        echo json_encode($json, http_response_code($json["status"]));
+        
+    }
+
+    /* Petición POST */
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        
+        $json = array(
+
+            'status' => 202,
+            'result' => 'Solicitud POST'
+        
+        );
+        echo json_encode($json, http_response_code($json["status"]));
+        
+    }
+
+    /* Petición PUT */
+
+    if($_SERVER['REQUEST_METHOD'] == "PUT"){
+        
+        $json = array(
+
+            'status' => 202,
+            'result' => 'Solicitud PUT'
+        
+        );
+        echo json_encode($json, http_response_code($json["status"]));
+        
+    }
+
+    /* Petición DELETE */
+
+    if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+        
+        $json = array(
+
+            'status' => 202,
+            'result' => 'Solicitud DELETE'
+        
+        );
+        echo json_encode($json, http_response_code($json["status"]));
+        
+    }
+
+
+
+
+}
+
 
 
