2e7882ce771403872759a65dad746050965e5463
josemg95 <foxe1020@gmail.com>
2024-10-25 02:12:07 -0500

    Add files via upload
    se actualiza hasta el video 10

---
+++ controllers/get.controller.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "models/get.model.php";
+
+class GetController{
+
+
+    static public function getData($table){
+
+        $response = GetModel::getData($table);
+        return $response;
+    }
+}
