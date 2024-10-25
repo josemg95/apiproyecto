2b19fbb01e685b90f1bc8de8cc543beae2e921db
josemg95 <foxe1020@gmail.com>
2024-10-25 00:08:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 ini_set("log_errors", 1);
-ini_set("error_log", "C:/xampp\htdocs\apiproyecto/error_log");
+ini_set("error_log", "C:/xampp\htdocs/apiproyecto/error_log");
 
 
 //Requerimiento de la ruta princupal
